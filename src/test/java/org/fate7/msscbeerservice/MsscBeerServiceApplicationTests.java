afa8ca199094e38b4c99f858cc6c8b6331cb9610
Fateh Boucenna <boucenna.fateh@gmail.com>
2022-02-27 23:47:17 +0100

    Initial project added.

---
+++ src/test/java/org/fate7/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fate7.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
