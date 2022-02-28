6a2af0ae72ab1d990366efb48fb233d654ac2059
Fateh Boucenna <boucenna.fateh@gmail.com>
2022-02-28 11:17:58 +0100

    Adding web data model. closes #2

---
+++ src/main/java/org/fate7/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,15 @@
+package org.fate7.msscbeerservice.web.model;
+
+public enum BeerStyle {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+    
+}
