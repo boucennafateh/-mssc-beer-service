5022bae4585c92592e30fd2c39c05a05570a8cf8
Fateh Boucenna <boucenna.fateh@gmail.com>
2022-03-24 23:06:47 +0100

    adding Jackson config to BeerDto. closes #13

--- src/main/java/org/fate7/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/org/fate7/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package org.fate7.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -36,15 +37,18 @@ public class BeerDto {
 
     @NotNull
     @Positive
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
 
     @Null
     private Integer version;
 
     @Null
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
     private OffsetDateTime lastModifiedDate;
 
 }
